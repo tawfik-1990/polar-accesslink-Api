021fba01e615e7c596f582efcb1ee3ade9332260
tawfikbouguerba <tawfikbouguerba13@gmail.com>
2018-03-12 13:50:51 +0100

    commit

--- webservice_for_Polar_Api/routes/router.js
+++ webservice_for_Polar_Api/routes/router.js
@@ -23,7 +23,8 @@ app.post('/heart/:userid', function(req,res)
 {
    var userid = req.params.userid;
   if (!userid || userid === "") {
-    return res.json({ "success": false, "msg": "userid is false", "error": err });
+
+    return res.status(400).send({ "success": false, "msg": "userid is false", "error": err })
   }
 Get_Cle_Secret(userid)
 
@@ -466,3 +467,41 @@ mongo.connect(url, function(err, db) {
 
 
 
+function insert_heart_tobase(userid,heartrate)
+{
+
+ return new Promise( function( resolve, reject ){
+ var item = {
+    userid: userid,
+   heart_rate: heartrate
+  };
+mongo.connect(url, function(err, db) {
+    if (err)
+    {
+      reject(err);
+    }
+    else
+    {
+     var dbo = db.db("usertoken");
+     var query = { userid: userid };
+  dbo.collection("user").insertOne(item, function(err, result) {
+  if(err)
+      {
+        reject(err);
+      }
+     else
+     {
+      resolve('Item inserted');
+      db.close();
+     }
+    
+     
+
+      })
+     
+   
+    }
+  });
+  });
+
+}
