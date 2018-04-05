87c0b9eebdd0e26eaaac4cbfa8512bc7b9b54877
Bouguerba, Tawfik <tawfik.bouguerba@stud.hs-ruhrwest.de>
2018-04-05 14:03:09 +0200

    Last commit

--- Get_access_Token/server.js
+++ Get_access_Token/server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 var bodyParser = require("body-parser");
 var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
@@ -20,7 +24,48 @@ app.set('view engine', 'handlebars');
 
 
 
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
 
+
+
+
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+
+
+app.use(flash());
+
+
+
+
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 
 
